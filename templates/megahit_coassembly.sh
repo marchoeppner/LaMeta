bef4d28f1f791f4823f7e63917e16c976c6854dc
mruehlemann <malte.ruehlemann@gmail.com>
2018-04-18 22:09:54 +0200

    tt

--- templates/megahit_coassembly.sh
+++ templates/megahit_coassembly.sh
@@ -4,8 +4,8 @@ do
 echo \$sample >> tmp1
 done
 
-awk '{printf " -1 " \$3 " -2 " \$4 " -r " \$5}' tmp1 > tmp
+awk '{printf " -1 " \$3 " -2 " \$4 " -r " \$5}' tmp1 > $outcontigs
 
-echo "$MEGAHIT \$(cat tmp | tr -d '\n') --num-cpu-threads ${task.cpus} --presets meta-large -o megahit_out --mem-flag 2 --verbose
-mv megahit_out/final.contigs.fa $outcontigs
-mv megahit_out/log $megahitlog"
+#echo "$MEGAHIT \$(cat tmp | tr -d '\n') --num-cpu-threads ${task.cpus} --presets meta-large -o megahit_out --mem-flag 2 --verbose
+#mv megahit_out/final.contigs.fa $outcontigs
+#mv megahit_out/log $megahitlog"
